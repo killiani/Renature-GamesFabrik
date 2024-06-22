04434693614c6d5fc8ef8ec9a28a445e8ac67059
killiani <59772495+killiani@users.noreply.github.com>
2024-06-22 12:31:09 +0200

    add Night trigger zone, add watch night animation for Pitti

---
+++ Assets/Scripts/Helpers/NightZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class NightZone : MonoBehaviour
+{
+    public Animator pittiAnimator; // Referenz zum Animator von Pitti
+
+    void Start()
+    {
+        if (pittiAnimator == null)
+        {
+            Debug.LogError("Animator not assigned in the inspector.");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("NightMarker"))
+        {
+            TriggerNightEvent();
+        }
+    }
+
+    void TriggerNightEvent()
+    {
+        pittiAnimator.SetTrigger("IsNight");
+    }
+}
