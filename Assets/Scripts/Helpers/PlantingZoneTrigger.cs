4c2cf0d3965c71a37d3a8def6fa75d745d3ed1e5
killiani <59772495+killiani@users.noreply.github.com>
2024-06-28 12:03:29 +0200

    add planting Zone dummy

---
+++ Assets/Scripts/Helpers/PlantingZoneTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlantingZoneTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Block block = other.GetComponent<Block>();
+        if (block != null)
+        {
+            // Hier könnte eine andere Logik stehen
+            // block.DeactivateGlow(); -> entfernen oder anpassen
+            //Debug.Log("Pitti entered a block.");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Block block = other.GetComponent<Block>();
+        if (block != null)
+        {
+            // Hier könnte eine andere Logik stehen
+            // block.DeactivateGlow(); -> entfernen oder anpassen
+            //Debug.Log("Pitti left a block.");
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Block.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Color objectGlowColor = Color.red;
+    private Color originalColor;
+    private bool hasObjectAbove = false;
+    public float checkHeight = 2f; // Height to check above
+    public float checkWidth = 1f; // Width of the BoxCast
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+        }
+
+        // Initial check for objects above
+        CheckForObjectsAbove();
+        UpdateColor();
+    }
+
+    private void CheckForObjectsAbove()
+    {
+        Collider2D[] hits = Physics2D.OverlapBoxAll(
+            new Vector2(transform.position.x, transform.position.y + checkHeight / 2),
+            new Vector2(checkWidth, checkHeight),
+            0f,
+            LayerMask.GetMask("Objects"));
+
+        hasObjectAbove = (hits.Length > 0);
+    }
+
+    private void UpdateColor()
+    {
+        if (spriteRenderer != null)
+        {
+            if (hasObjectAbove)
+            {
+                spriteRenderer.color = objectGlowColor;
+            }
+            else
+            {
+                spriteRenderer.color = originalColor;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (hasObjectAbove)
+            {
+                Debug.Log("Pitti is on a red block.");
+            }
+            else
+            {
+                Debug.Log("Pitti is on a non-red block.");
+            }
+        }
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Objects"))
+        {
+            hasObjectAbove = true;
+            UpdateColor();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Pitti has left the block.");
+        }
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Objects"))
+        {
+            hasObjectAbove = false;
+            UpdateColor();
+        }
+    }
+
+    void Update()
+    {
+        CheckForObjectsAbove();
+        UpdateColor();
+    }
+}
